3da23e8a935828e882c3c2c14cbfc5bdbf72e5ef
kdawsn133 <kdawson133@gmail.com>
2019-03-19 17:47:30 +1100

    second commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+gulp.task('watch', function(){
+  gulp.watch('scss/**/*.scss', ['sass']);
+})
+
+gulp.task('sass', function(){
+  return gulp.src('scss/styleshell.scss')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('css'))
+    .pipe(minifyCss())
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(gulp.dest('css'))
+});
